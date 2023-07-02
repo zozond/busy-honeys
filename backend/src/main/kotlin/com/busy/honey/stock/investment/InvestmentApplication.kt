613115183cef669c2c9fa02ec3107ef15247f1d4
jyotti <zozond@naver.com>
2023-07-02 17:04:53 +0900

    config: 백엔드 및 프로젝트 구조 셋팅

---
+++ backend/src/main/kotlin/com/busy/honey/stock/investment/InvestmentApplication.kt
@@ -0,0 +1,11 @@
+package com.busy.honey.stock.investment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvestmentApplication
+
+fun main(args: Array<String>) {
+	runApplication<InvestmentApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/busy/honey/stock/investment/InvestmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.busy.honey.stock.investment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InvestmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
